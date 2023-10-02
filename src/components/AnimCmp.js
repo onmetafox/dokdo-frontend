22c40c8058d47fcb51ef9f4271015592bdf5545e
onmetafox <onemetafox@outlook.com>
2023-10-02 05:22:28 -0700

    fix: side menu button error

--- src/components/AnimCmp.js
+++ src/components/AnimCmp.js
@@ -2,10 +2,10 @@ import React from "react";
 import circle1 from "../assets/images/circle1.png";
 import circle2 from "../assets/images/circle2.png";
 
-import { Box } from "@mui/material";
+import { Box, Hidden } from "@mui/material";
 
 const AnimComp = () => {
-    return <Box sx={{position:"relative", width:'100%'}}>
+    return <Box sx={{position:"relative",overflow:'hidden', width:'100%'}}>
         <Box component="img" src={circle1} sx={{position:'absolute', width:'100%'}}></Box>
         <Box component="img" src={circle2} sx={{width: '100%'}} className="rotating-element"></Box>
     </Box>
