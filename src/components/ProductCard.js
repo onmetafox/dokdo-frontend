8492b0e9c2b34c9cf54f3099c7a1c278bca8df48
onmetafox <onemetafox@outlook.com>
2023-12-18 21:25:09 +0400

    fix: remove read more button

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,11 @@
 import { Box, Grid } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import Button from "./Button";
-import Link from "./Link";
+// import Link from "./Link";
 
 const ProductCard = (props) => {
-    const [visible, setVisible] = useState(true)
-    const { image, title, detail, button, link} = props.data;
+    // const [visible, setVisible] = useState(true)
+    const { image, title, detail, button} = props.data;
     let direct = true;
     if(props.index % 2 === 0 ){
         direct = false;
@@ -21,10 +21,10 @@ const ProductCard = (props) => {
                     <Button className = "product-button m-tb-10 no-cursor" title= {button}/>
                     <Box className= "t-p h5 m-tb-10"> {title} </Box>
                     <Box className= "t-p m-tb-10"> {detail} </Box>
-                    <Box onMouseEnter={() =>{if(button !== "Game") setVisible(false)}} onMouseLeave={() => {if(button !== "Game") setVisible(true)}} sx={{display:'flex', width:'fit-content'}}>
+                    {/* <Box onMouseEnter={() =>{if(button !== "Game") setVisible(false)}} onMouseLeave={() => {if(button !== "Game") setVisible(true)}} sx={{display:'flex', width:'fit-content'}}>
                         {visible && <Link href= {link} className="t-o no-line" title="Read more" icon={true} status="active"/>}
                         {!visible && <Box className="t-o no-line ext-link" sx={{cursor:"pointer"}}>Coming soon</Box>}
-                    </Box>
+                    </Box> */}
                 </Grid>
             </>
             )}
@@ -33,10 +33,10 @@ const ProductCard = (props) => {
                     <Button className = "product-button m-tb-10 no-cursor" title= {button}/>
                     <Box className= "t-p h5 m-tb-10"> {title} </Box>
                     <Box className= "t-p m-tb-10"> {detail} </Box>
-                    <Box onMouseEnter={() => {if(button !== "Game") setVisible(false)}} onMouseLeave={() => {if(button !== "Game") setVisible(true)}} sx={{display:'flex', width:'fit-content'}}>
+                    {/* <Box onMouseEnter={() => {if(button !== "Game") setVisible(false)}} onMouseLeave={() => {if(button !== "Game") setVisible(true)}} sx={{display:'flex', width:'fit-content'}}>
                         {visible && <Link href= {link} className="t-o no-line" title="Read more" icon={true} status="active" />}
                         {!visible && <Box className="t-o no-line ext-link" sx={{cursor:"pointer"}}>Coming soon</Box>}
-                    </Box>
+                    </Box> */}
                 </Grid>
                 <Grid item lg={6}>
                     <Box component="img" src={image} sx={{width:'80%', marginLeft:2}}></Box>
